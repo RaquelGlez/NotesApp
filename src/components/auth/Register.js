1ed3d19cccf299f553843eee887a875356d5e647
raquel gonzalez <gonzalez.c.raquel@gmail.com>
2021-03-02 13:47:23 -0600

    register styles added

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const Register = () => {
-    return (
-        <div>
-            <h1>Register!!!!!!!!!!!</h1>
+  return (
+    <div>
+      <h3 className="auth__title">Register</h3>
+      <form>
+      <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          className="app__input"
+          autoComplete="off"
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          className="app__input"
+          autoComplete="off"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          className="app__input"
+        />
+         <input
+          type="password"
+          placeholder="Confirm password"
+          name="password2"
+          className="app__input"
+        />
+        <button type="submit" className="btn btn-block btn-primary mb-5">
+          Register
+        </button>
+        <div className="auth__link">
+          <Link className="link" to="/auth/login">
+            Already registered?
+          </Link>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
