44d38679024fb2e54bdca67b7106e1086e943bed
raquel gonzalez <gonzalez.c.raquel@gmail.com>
2021-04-21 15:42:54 -0500

    added notes reducer

--- src/components/notes/Notes.js
+++ src/components/notes/Notes.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { NoteScreen } from '../manageNotes/NoteScreen';
 import {Sidebar} from './Sidebar'
+import { useSelector } from 'react-redux';
 import { WithoutSelection } from './WithoutSelection';
 
 
-// esto es el journalScreen
+// journalScreen
 export const Notes = () => {
+
+    const { active } = useSelector(state => state.notes)
+
     return (
         <div className="notes__main-content">
             
             <Sidebar />
 
             <main>
-                {/* <WithoutSelection/> */}
-                <NoteScreen/>
+
+                { 
+                    ( active )
+                        ? ( <NoteScreen/> ) 
+                        : ( <WithoutSelection/> )  
+                }
 
             </main>
         </div>
     )
 }
+
+
