a16623db3117a50b4f9e24d940cc11b3a0a4112c
raquel gonzalez <gonzalez.c.raquel@gmail.com>
2021-04-26 18:18:32 -0500

    the current note is updated and saved

--- src/components/manageNotes/NoteScreen.js
+++ src/components/manageNotes/NoteScreen.js
@@ -1,25 +1,31 @@
 import React, { useEffect, useRef } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { activeNote } from "../../actions/notes";
 import { useForm } from "../../hooks/useForm";
 import { NotesAppBar } from "./NotesAppBar";
 
 export const NoteScreen = () => {
-  const { active: note } = useSelector((state) => state.notes);
-  
-  const [formValues, handleInputChange, reset] = useForm(note);
 
+  const dispatch = useDispatch();
+
+  const { active: note } = useSelector((state) => state.notes);
+  const [formValues, handleInputChange, reset] = useForm(note);
   const { body, title } = formValues;
 
   const activeId = useRef( note.id )
 
   useEffect(() => {
-    
     if( note.id !== activeId.current ){
       reset( note )
       activeId.current = note.id
     }
 
   }, [reset, note])
+
+  useEffect(() => {
+    dispatch( activeNote(formValues.id, {...formValues}) )
+
+  }, [formValues, dispatch]);
 
   return (
     <div className="noteScreen_main-content">
@@ -28,6 +34,7 @@ export const NoteScreen = () => {
       <div className="noteScreen__content">
         <input
           type="text"
+          name="title"
           placeholder="Some awesome title"
           className="noteScreen__title-input"
           autoComplete="off"
@@ -36,6 +43,7 @@ export const NoteScreen = () => {
         />
         <textarea
           placeholder="What happened today"
+          name="body"
           className="noteScreen__textarea"
           value={body}
           onChange={handleInputChange}
